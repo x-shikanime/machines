4b27fd893205587a01fe53033c3dfd0468e011d6
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-06-05 23:48:01 +0200

    Update
    [ghstack-poisoned]

--- hosts/manash/configuration.nix
+++ hosts/manash/configuration.nix
@@ -179,6 +179,7 @@
       "--cluster-cidr=10.42.0.0/16,2001:cafe:42::/56"
       "--secrets-encryption"
       "--service-cidr=10.43.0.0/16,2001:cafe:43::/112"
+      "--node-ip=100.74.220.28,fd7a:115c:a1e0::8d3a:dc1c"
       "--cni=multus"
       "--cni=canal"
     ];
