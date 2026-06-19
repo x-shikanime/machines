4222c28427b7fddca6253709f2583f83cfeb84c2
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-06-19 22:13:43 +0200

    Update
    [ghstack-poisoned]

--- modules/nixos/nishir.nix
+++ modules/nixos/nishir.nix
@@ -15,30 +15,28 @@
     ];
   };
 
-  disko.devices = {
-    disk.main = {
-      type = "disk";
-      device = "/dev/nvme0n1";
-      content = {
-        type = "gpt";
-        partitions = {
-          ESP = {
-            size = "1G";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-              mountOptions = [ "umask=0077" ];
-            };
+  disko.devices.disk.main = {
+    type = "disk";
+    device = "/dev/nvme0n1";
+    content = {
+      type = "gpt";
+      partitions = {
+        ESP = {
+          size = "1G";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+            mountOptions = [ "umask=0077" ];
           };
-          root = {
-            size = "100%";
-            content = {
-              type = "filesystem";
-              format = "xfs";
-              mountpoint = "/";
-            };
+        };
+        root = {
+          size = "100%";
+          content = {
+            type = "filesystem";
+            format = "xfs";
+            mountpoint = "/";
           };
         };
       };
@@ -194,11 +192,9 @@
       rke2-token.restartUnits = [ "rke2-server.service" ];
       tailscale-authkey.restartUnits = [ "tailscaled.service" ];
     };
-    templates = {
-      nix-config.content = ''
-        extra-access-tokens = "github.com=${config.sops.placeholder.nix-access-token}";
-      '';
-    };
+    templates.nix-config.content = ''
+      extra-access-tokens = "github.com=${config.sops.placeholder.nix-access-token}";
+    '';
   };
 
   virtualisation.docker = {
