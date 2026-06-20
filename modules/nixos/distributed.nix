c487ae2c18d254f91c485f796d5520a220dee6d9
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-06-20 23:48:06 +0200

    Update
    [ghstack-poisoned]

--- modules/nixos/distributed.nix
+++ modules/nixos/distributed.nix
@@ -5,12 +5,12 @@
     settings = {
       builders-use-substitutes = true;
       substituters = [
-        "http://ashira.taila659a.ts.net:5000"
-        "http://manash.taila659a.ts.net:5000"
-        "http://nalsha.taila659a.ts.net:5000"
-        "http://fushi.taila659a.ts.net:5000"
-        "http://minish.taila659a.ts.net:5000"
-        "http://nemishi.taila659a.ts.net:5000"
+        "https://ashira.taila659a.ts.net:5000"
+        "https://manash.taila659a.ts.net:5000"
+        "https://nalsha.taila659a.ts.net:5000"
+        "https://fushi.taila659a.ts.net:5000"
+        "https://minish.taila659a.ts.net:5000"
+        "https://nemishi.taila659a.ts.net:5000"
       ];
     };
 
