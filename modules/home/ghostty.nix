93ba569d06647c9a08b3958b14eef56e6726e0d2
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-06-27 14:11:57 +0200

    Update
    [ghstack-poisoned]

--- modules/home/ghostty.nix
+++ modules/home/ghostty.nix
@@ -12,7 +12,7 @@ with lib;
     enable = true;
     package = if pkgs.stdenv.isLinux then pkgs.ghostty else pkgs.ghostty-bin;
     settings = {
-      theme = "dark:catppuccin-frappe,light:catppuccin-latte";
+      theme = mkForce "dark:catppuccin-frappe,light:catppuccin-latte";
       command = "${getExe pkgs.zsh} -c ${getExe pkgs.nushell}";
     };
   };
