da9d1c7573a3eeb677928e767a0e52ba34c81fad
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-06-28 14:54:14 +0200

    Update
    [ghstack-poisoned]

--- modules/nixos/node.nix
+++ modules/nixos/node.nix
@@ -88,4 +88,46 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH+tp1Xfz7NomHCZuDPlfj3XW5hm9t0TiCyEeudRraoe"
     ];
   };
+
+  users.users.builder = {
+    isNormalUser = true;
+    home = "/home/builder";
+    useDefaultShell = true;
+  };
+
+  virtualisation.docker = {
+    daemon.settings = {
+      fixed-cidr-v6 = "fd00::/80";
+      ipv6 = true;
+    };
+    enable = true;
+  };
+
+  services.gitea-actions-runner = {
+    package = pkgs.forgejo-runner;
+    instances = {
+      forgejo = {
+        enable = true;
+        name = config.networking.hostName;
+        tokenFile = config.sops.templates.forgejo-runner-token.path;
+        url = "https://forgejo.taila659a.ts.net";
+        labels = [
+          "docker:docker://node:22-bookworm"
+          "nixos-latest:docker://nixos/nix"
+          "native:host"
+        ];
+      };
+    };
+  };
+
+  sops = {
+    secrets = {
+      forgejo-runner-token.restartUnits = [ "forgejo-runner-${config.networking.hostName}.service" ];
+    };
+    templates = {
+      forgejo-runner-token.content = ''
+        TOKEN=${config.sops.placeholder.forgejo-runner-token}
+      '';
+    };
+  };
 }
--- modules/nixos/server.nix
+++ modules/nixos/server.nix
@@ -6,32 +6,16 @@
   ];
 
   services = {
-    gitea-actions-runner = {
-      package = pkgs.forgejo-runner;
-      instances = {
-        codeberg = {
-          enable = true;
-          name = config.networking.hostName;
-          tokenFile = config.sops.templates.codeberg-runner-token.path;
-          url = "https://codeberg.org";
-          labels = [
-            "docker:docker://node:22-bookworm"
-            "nixos-latest:docker://nixos/nix"
-            "native:host"
-          ];
-        };
-        forgejo = {
-          enable = true;
-          name = config.networking.hostName;
-          tokenFile = config.sops.templates.forgejo-runner-token.path;
-          url = "https://forgejo.taila659a.ts.net";
-          labels = [
-            "docker:docker://node:22-bookworm"
-            "nixos-latest:docker://nixos/nix"
-            "native:host"
-          ];
-        };
-      };
+    gitea-actions-runner.instances.codeberg = {
+      enable = true;
+      name = config.networking.hostName;
+      tokenFile = config.sops.templates.codeberg-runner-token.path;
+      url = "https://codeberg.org";
+      labels = [
+        "docker:docker://node:22-bookworm"
+        "nixos-latest:docker://nixos/nix"
+        "native:host"
+      ];
     };
 
     knix = {
@@ -114,29 +98,12 @@
   sops = {
     secrets = {
       codeberg-runner-token.restartUnits = [ "codeberg-runner-${config.networking.hostName}.service" ];
-      forgejo-runner-token.restartUnits = [ "forgejo-runner-${config.networking.hostName}.service" ];
     };
     templates = {
       codeberg-runner-token.content = ''
         TOKEN=${config.sops.placeholder.codeberg-runner-token}
       '';
-      forgejo-runner-token.content = ''
-        TOKEN=${config.sops.placeholder.forgejo-runner-token}
-      '';
     };
   };
 
-  users.users.builder = {
-    isNormalUser = true;
-    home = "/home/builder";
-    useDefaultShell = true;
-  };
-
-  virtualisation.docker = {
-    daemon.settings = {
-      fixed-cidr-v6 = "fd00::/80";
-      ipv6 = true;
-    };
-    enable = true;
-  };
 }
