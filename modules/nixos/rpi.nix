d3e6f354538a4cd7ea6ed5dbc388734f5aa8d25a
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-06-29 20:14:19 +0200

    Update
    [ghstack-poisoned]

---
+++ modules/nixos/rpi.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }:
+
+{
+  # Older Raspberry Pi-class boards still need these cgroup knobs for RKE2.
+  boot.kernelParams = [
+    "cgroup_enable=cpuset"
+    "cgroup_enable=memory"
+    "cgroup_memory=1"
+  ];
+
+  nixpkgs.overlays = [
+    (_: super: {
+      makeModulesClosure = x: super.makeModulesClosure (x // { allowMissing = true; });
+    })
+  ];
+
+  services.tailscale-udp-gro-forwarding = {
+    after = [ "network-online.target" ];
+    description = "Enable Tailscale UDP GRO forwarding on end0";
+    script = ''
+      ${pkgs.ethtool}/bin/ethtool -K end0 rx-udp-gro-forwarding on rx-gro-list off
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      Restart = "on-failure";
+      RestartSec = "5s";
+    };
+    wantedBy = [ "multi-user.target" ];
+    wants = [ "network-online.target" ];
+  };
+}
