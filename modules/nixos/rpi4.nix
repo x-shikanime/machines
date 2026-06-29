01425fb3bdf9116edd63fd9da4fc0f60967f0e5c
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-06-29 19:51:49 +0200

    Split RPI configs
    Signed-off-by: Shikanime Deva <william.phetsinorath@shikanime.studio>
    Change-Id: Ia37375d9f530cf115e8a0b2103ad15ae6a6a6964
    ghstack-source-id: 23e9d1c6749e9a47dc426913311e00e4ee6d8240
    Pull-Request: https://github.com/x-shikanime/machines/pull/1047

---
+++ modules/nixos/rpi4.nix
@@ -0,0 +1,16 @@
+{
+  # Older Raspberry Pi-class boards still need these cgroup knobs for RKE2.
+  boot.kernelParams = [
+    "cgroup_enable=cpuset"
+    "cgroup_enable=memory"
+    "cgroup_memory=1"
+  ];
+
+  nixpkgs.overlays = [
+    (_: super: {
+      makeModulesClosure = x: super.makeModulesClosure (x // { allowMissing = true; });
+    })
+  ];
+
+  hardware.raspberry-pi."4".fkms-3d.enable = true;
+}
