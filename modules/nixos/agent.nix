2d1a991962db9811f695331893e1df771e64453c
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-06-29 22:50:25 +0200

    Update
    [ghstack-poisoned]

--- modules/nixos/agent.nix
+++ modules/nixos/agent.nix
@@ -8,7 +8,7 @@
   services.knix = {
     enable = true;
     role = "agent";
-    serverAddr = "https://192.168.1.28:9345";
+    serverAddr = "https://nishir.taila659a.ts.net:9345";
     tokenFile = config.sops.secrets.rke2-token.path;
   };
 
--- modules/nixos/server.nix
+++ modules/nixos/server.nix
@@ -6,7 +6,7 @@
   ];
 
   services.knix = {
-    serverAddr = "https://192.168.1.28:9345";
+    serverAddr = "https://nishir.taila659a.ts.net:9345";
     tokenFile = config.sops.secrets.rke2-token.path;
   };
 
