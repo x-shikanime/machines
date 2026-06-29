556b8b4e5c46a1f113f74edd58a55b2b50e9e06a
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-06-29 18:44:01 +0200

    Update
    [ghstack-poisoned]

--- hosts/manash/configuration.nix
+++ hosts/manash/configuration.nix
@@ -40,6 +40,7 @@
     secrets = {
       codeberg-runner-token.sopsFile = ../../secrets/nishir.enc.yaml;
       forgejo-runner-token.sopsFile = ../../secrets/nishir.enc.yaml;
+      nix-access-token.sopsFile = ../../secrets/nishir.enc.yaml;
       wifi-sfr-e368.sopsFile = ../../secrets/nishir.enc.yaml;
       wifi-sfr-e368-5ghz.sopsFile = ../../secrets/nishir.enc.yaml;
       wifi-vintage-korean.sopsFile = ../../secrets/nishir.enc.yaml;
