6dc23442102e37165b2c17d8a0f80be7a3310a49
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-07-01 21:47:27 +0200

    Backport RPI UAS mitiguation
    Signed-off-by: Shikanime Deva <william.phetsinorath@shikanime.studio>
    Change-Id: Ic98d5cf27166824e8760a4c1f824929a6a6a6964

--- modules/nixos/rpi4.nix
+++ modules/nixos/rpi4.nix
@@ -4,4 +4,16 @@
   ];
 
   hardware.raspberry-pi."4".fkms-3d.enable = true;
+
+  # VL805 xHCI stability mitigations
+  boot.kernelParams = [
+    # Disable xHCI streams - causes "unknown stream ring" errors on VL805
+    "xhci-hcd.quirk_usb2_inst_suspend=0"
+    # Disable ASPM to reduce PCIe link power transitions
+    "pcie_aspm=off"
+  ];
+
+  # Disable UAS for external USB drives - use more stable usb-storage
+  # Blacklist UAS kernel module to prevent crashes on VL805
+  boot.blacklistedKernelModules = [ "uas" ];
 }
